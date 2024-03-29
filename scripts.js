4945917df9f4eac37b78656838b635acb9b3796f
bvandyke <bvandyke00@gmail.com>
2024-03-29 12:12:27 -0400

    FIRST COMMIT

---
+++ scripts.js
@@ -0,0 +1,12 @@
+
+// ------ nav slider -------- //
+
+const btn = document.getElementById('slider-toggle')
+.addEventListener('click', event => {
+    const slider = document.getElementById('slider-menu');
+    slider.classList.toggle('hide-menu');
+});
+
+
+// --------- Modal --------- //
+
